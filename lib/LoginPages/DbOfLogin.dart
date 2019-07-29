d880584c63efe901ba32b7a03c9fcd8d60462560
break a leg <nonahuda26@gmail.com>
2019-07-29 14:21:21 +0300

    the first start

--- lib/LoginPages/DbOfLogin.dart
+++ lib/LoginPages/DbOfLogin.dart
@@ -1,22 +1,60 @@
-import 'package:firebase_database/firebase_database.dart';
-//i want to sent the information of users as widget
-import 'package:flutter/widgets.dart';
-// i add the users which i want to send  them to database in class
-class UserToDatabase{
-  //function to help me to import it from any where
-  addNewUser(user,context){
-    FirebaseDatabase.instance.reference().child('users').push().set({
-      'email':user.email,
-      'uid':user.uid,
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-      //i use then to do many thinks such as move it to another page
-    }).then((value){
-      Navigator.of(context).pop();
-      Navigator.of(context).pushReplacementNamed('/home');
-    }).catchError((e){
-      print(e);
-    });
-  }
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
+Future<String> signInWithGoogle() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+  await googleSignInAccount.authentication;
 
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final FirebaseUser user = await _auth.signInWithCredential(credential);
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(user.uid == currentUser.uid);
+
+  return 'signInWithGoogle succeeded: $user';
 }
+
+void signOutGoogle() async{
+  await googleSignIn.signOut();
+
+  print("User Sign Out");
+}
+
+
+
+
+
+
+//import 'package:firebase_database/firebase_database.dart';
+////i want to sent the information of users as widget
+//import 'package:flutter/widgets.dart';
+//// i add the users which i want to send  them to database in class
+//class UserToDatabase{
+//  //function to help me to import it from any where
+//  addNewUser(user,context){
+//    FirebaseDatabase.instance.reference().child('users').push().set({
+//      'email':user.email,
+//      'uid':user.uid,
+//
+//      //i use then to do many thinks such as move it to another page
+//    }).then((value){
+//      Navigator.of(context).pop();
+//      Navigator.of(context).pushReplacementNamed('/home');
+//    }).catchError((e){
+//      print(e);
+//    });
+//  }
+//
+//
+//}
--- lib/LoginPages/LoginPage.dart
+++ lib/LoginPages/LoginPage.dart
@@ -1,109 +1,240 @@
 import 'package:flutter/material.dart';
-void main() => runApp(new LoginPage());
+import 'package:mmsfas2_flu/Lectures_stu/Lectures_list.dart';
+import 'package:mmsfas2_flu/LoginPages/DbOfLogin.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({Key key}) : super(key: key);
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-//  _formKey and _autoValidate
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  bool _autoValidate = false;
-  String _email;
-  String _PassWord;
-
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Form Validation'),
-        ),
-        body: new SingleChildScrollView(
-          child: new Container(
-            margin: new EdgeInsets.all(15.0),
-            child: new Form(
-              key: _formKey,
-              autovalidate: _autoValidate,
-              child: FormUI(),
-            ),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _signInButton(),
+            ],
           ),
         ),
       ),
     );
   }
 
-// Here is our Form UI
-  Widget FormUI() {
-    return new Column(
-      children: <Widget>[
-        new TextFormField(
-          decoration: const InputDecoration(labelText: 'Email'),
-          keyboardType: TextInputType.emailAddress,
-          validator: validateEmail,
-          onSaved: (String val) {
-            _email = val;
-          },
-        ),
-        new TextFormField(
-          decoration: const InputDecoration(labelText: 'Password'),
-          keyboardType: TextInputType.phone,
-          validator: validatePassWord,
-          onSaved: (String val) {
-            _PassWord = val;
-          },
-        ),
 
-        new SizedBox(
-          height: 10.0,
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().whenComplete(() {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return  Homepage();
+              },
+            ),
+          );
+        });
+      },
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("img/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
         ),
-        new RaisedButton(
-          onPressed: _validateInputs,
-          child: new Text('Validate'),
-        )
-      ],
+      ),
     );
   }
+}
 
-  String validateName(String value) {
-    if (value.length < 8)
-      return 'Name must be more than 7 charater';
-    else
-      return null;
-  }
 
-  String validatePassWord(String value) {
-// Indian Mobile number are of 10 digit only
-    if (value.length != 6)
-      return 'Mobile Number must be of 6 digit';
-    else
-      return null;
-  }
 
-  String validateEmail(String value) {
-    Pattern pattern =
-        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-    RegExp regex = new RegExp(pattern);
-    if (!regex.hasMatch(value))
-      return 'Enter Valid Email';
-    else
-      return null;
-  }
 
-  void _validateInputs() {
-    if (_formKey.currentState.validate()) {
-//    If all data are correct then save data to out variables
-      _formKey.currentState.save();
-    } else {
-//    If all data are not valid then start auto validation.
-      setState(() {
-        _autoValidate = true;
-      });
-    }
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import 'package:flutter/material.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+//void main() => runApp(new LoginPage());
+//
+//class LoginPage extends StatefulWidget {
+//  LoginPage({Key key}) : super(key: key);
+//
+//  @override
+//  _LoginPageState createState() => _LoginPageState();
+//}
+//
+//class _LoginPageState extends State<LoginPage> {
+////  _formKey and _autoValidate
+//  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+//  bool _autoValidate = false;
+//  String _email;
+//  String _PassWord;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new MaterialApp(
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('Form Validation'),
+//        ),
+//        body: new SingleChildScrollView(
+//          child: new Container(
+//            margin: new EdgeInsets.all(15.0),
+//            child: new Form(
+//              key: _formKey,
+//              autovalidate: _autoValidate,
+//              child: FormUI(),
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//
+//// Here is our Form UI
+//  Widget FormUI() {
+//    return new Column(
+//      children: <Widget>[
+//        new TextFormField(
+//          decoration: const InputDecoration(labelText: 'Email'),
+//          keyboardType: TextInputType.emailAddress,
+//          validator: validateEmail,
+//          onSaved: (String val) {
+//            _email = val;
+//          },
+//        ),
+//        new TextFormField(
+//          decoration: const InputDecoration(labelText: 'Password'),
+//          keyboardType: TextInputType.phone,
+//          validator: validatePassWord,
+//          onSaved: (String val) {
+//            _PassWord = val;
+//          },
+//        ),
+//
+//        new SizedBox(
+//          height: 10.0,
+//        ),
+//        new RaisedButton(
+//          onPressed: _validateInputs,
+//          child: new Text('Validate'),
+//        )
+//      ],
+//    );
+//  }
+//
+//  String validateName(String value) {
+//    if (value.length < 8)
+//      return 'Name must be more than 7 charater';
+//    else
+//      return null;
+//  }
+//
+//  String validatePassWord(String value) {
+//// Indian Mobile number are of 10 digit only
+//    if (value.length != 6)
+//      return 'Mobile Number must be of 6 digit';
+//    else
+//      return null;
+//  }
+//
+//  String validateEmail(String value) {
+//    Pattern pattern =
+//        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+//    RegExp regex = new RegExp(pattern);
+//    if (!regex.hasMatch(value))
+//      return 'Enter Valid Email';
+//    else
+//      return null;
+//  }
+//
+//  void _validateInputs() {
+//    if (_formKey.currentState.validate()) {
+////    If all data are correct then save data to out variables
+//      _formKey.currentState.save();
+//    } else {
+////    If all data are not valid then start auto validation.
+//      setState(() {
+//        _autoValidate = true;
+//      });
+//    }
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //import 'package:firebase_auth/firebase_auth.dart';
@@ -222,4 +353,4 @@ class _LoginPageState extends State<LoginPage> {
 //
 //
 //}
-}
+//}
