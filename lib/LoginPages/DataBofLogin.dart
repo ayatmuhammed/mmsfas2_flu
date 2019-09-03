8ce891f4935fecf568a5a8328c78954e2d418956
break a leg <nonahuda26@gmail.com>
2019-09-03 23:24:05 +0300

    data base for login by email

--- lib/LoginPages/DataBofLogin.dart
+++ lib/LoginPages/DataBofLogin.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -8,7 +9,6 @@ Future<String> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
   await googleSignInAccount.authentication;
-
   final AuthCredential credential = GoogleAuthProvider.getCredential(
     accessToken: googleSignInAuthentication.accessToken,
     idToken: googleSignInAuthentication.idToken,
@@ -33,28 +33,25 @@ void signOutGoogle() async{
 
 
 
+class Todo {
+  String key;
+  String subject;
+  bool completed;
+  String userId;
 
+  Todo(this.subject, this.userId, this.completed);
 
+  Todo.fromSnapshot(DataSnapshot snapshot) :
+        key = snapshot.key,
+        userId = snapshot.value["userId"],
+        subject = snapshot.value["subject"],
+        completed = snapshot.value["completed"];
 
-//import 'package:firebase_database/firebase_database.dart';
-////i want to sent the information of users as widget
-//import 'package:flutter/widgets.dart';
-//// i add the users which i want to send  them to database in class
-//class UserToDatabase{
-//  //function to help me to import it from any where
-//  addNewUser(user,context){
-//    FirebaseDatabase.instance.reference().child('users').push().set({
-//      'email':user.email,
-//      'uid':user.uid,
-//
-//      //i use then to do many thinks such as move it to another page
-//    }).then((value){
-//      Navigator.of(context).pop();
-//      Navigator.of(context).pushReplacementNamed('/home');
-//    }).catchError((e){
-//      print(e);
-//    });
-//  }
-//
-//
-//}
+  toJson() {
+    return {
+      "userId": userId,
+      "subject": subject,
+      "completed": completed,
+    };
+  }
+}
