252e3511665d5a6c93e51c4430301ab4c0c1183c
break a leg <nonahuda26@gmail.com>
2019-09-03 23:33:58 +0300

    splash screen

---
+++ lib/SplashScreen/loader.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Splash_Screen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: SpinKitPouringHourglass(
+            color: Colors.indigo,
+            size: 80.0,
+            duration:  Duration(seconds: 5),
+          ),
+
+        ),
+
+      ),
+    );
+  }
+
+}
